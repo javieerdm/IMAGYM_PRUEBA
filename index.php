0609f13a250f82feee8786b201be4462b459e42b
JORGE BELINCHÓN CUESTA <Jorge.Belinchon@alu.uclm.es>
2023-10-23 17:15:06 +0200

    corrección index.php

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
 			
 			<section>			
 					<div id="section1">
-						<a href="ropa-deportiva.php"><div id="bañadores"></div></a>
-						<a href="material.php"><div id="equipacion"></div></a>
+						<a href="ropa-deportiva.php"><div id="ropa"></div></a>
+						<a href="material.php"><div id="material"></div></a>
 					</div>
 					<div id="section2">
-						<a href="maquinas.php"><div id="entrenamiento"></div></a>
-						<a href="suplementos.php"><div id="aguas"></div></a>
+						<a href="maquinas.php"><div id="maquinas"></div></a>
+						<a href="suplementos.php"><div id="suplementos"></div></a>
 					</div>					
 			</section>
 			
