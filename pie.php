14aa371fc8d46696b02ac1c8b5fac8c060c92a96
JAVIER DÍAZ MEJÍA <Javier.Diaz34@alu.uclm.es>
2023-10-23 18:38:14 +0200

    implementar pie.php

---
+++ pie.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Pie de página</title>
+		<link rel="stylesheet" href="pie.css">
+	</head>	
+	<body>	
+		<footer>
+			<div id="footer">
+				<div id="pies">
+					<div id="pie1">				
+						<ul>
+							<li>Información</li>	
+							<li><a href="politica.php">Política de privacidad</a></li>
+							<li><a href="condiciones.php">Condiciones legales</a></li>
+						</ul>				
+					</div>
+					<div id="pie3">				
+						<ul>
+							<li>Métodos de pago</li>
+							<li><img src="imagenes\pagos\pagos.jpg"></li>						
+						</ul><br><br>						
+					</div>
+				</div>
+				<div id="creador">
+					<p id="mcs">© 2019 Página Web creada por GRUPO IMAGYM S.L.</p>
+					
+				</div><br>
+			</div>
+			
+		</footer>	
+	</body>
+</html>
---
+++ politica.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Nuestra Política de privacidad</title>
+		<link rel="stylesheet" href="principal.css">
+		<link rel="stylesheet" href="politicaycondiciones.css">
+
+	</head>	
+	<body>
+		<div id="body">		
+		
+			<?php include('cabecera.php');?>
+			
+			<section>			
+				<br>
+				<h1 id="politica">Política de privacidad</h1>
+				<hr class="linea2"><br>
+				<p>Tienda Swimmer informa a los usuarios del sitio web sobre su política respecto 
+				del tratamiento y protección de los datos de carácter personal de los usuarios
+				y clientes que puedan ser recabados por la navegación o contratación de 
+				servicios a través de su sitio web.</p>
+				<br>
+				<p>En este sentido, Empresa A garantiza el cumplimiento de la normativa 
+				vigente en materia de protección de datos personales, reflejada en la Ley 
+				Orgánica 15/1999 de 13 de diciembre, de Protección de Datos de Carácter 
+				Personal y en el Real Decreto 1720/2007, de 21 diciembre, por el que se 
+				aprueba el Reglamento de Desarrollo de la LOPD.</p>
+				<br>
+				<p>El uso de esta web implica la aceptación de esta política de privacidad.</p>
+				<br>
+				<p><b>Recogida, finalidad y tratamientos de datos</p></b>
+				<br>
+				<p>Tienda Swimmer tiene el deber de informar a los usuarios de su sitio web 
+				acerca de la recogida de datos de carácter personal que pueden llevarse a 
+				cabo, bien sea mediante el envío de correo electrónico o al cumplimentar 
+				los formularios incluidos en el sitio web. En este sentido, Tienda Swimmer 
+				será considerada como responsable de los datos recabados mediante los 
+				medios anteriormente descritos.</p>
+				<br>
+				<p>Las operaciones, gestiones y procedimientos técnicos que se realicen de 
+				forma automatizada o no automatizada y que posibiliten la recogida, el 
+				almacenamiento, la modificación, la transferencia y otras acciones sobre 
+				datos de carácter personal, tienen la consideración de tratamiento de datos 
+				personales.</p>
+				<br>
+				<p><b>Derechos de los usuarios</b></p>
+				<br>
+				<p>La Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de 
+				Carácter Personal concede a los interesados la posibilidad de ejercer una 
+				serie de derechos relacionados con el tratamiento de sus datos personales</p>
+				<br>
+				<p>En tanto en cuanto los datos del usuario son objeto de tratamiento por 
+				parte de Tienda Swimmer. Los usuarios podrán ejercer los derechos de acceso, 
+				rectificación, cancelación y oposición de acuerdo con lo previsto en la 
+				normativa legal vigente en materia de protección de datos personales.</p>
+				<br>
+				<p>Para hacer uso del ejercicio de estos derechos, el usuario deberá 
+				dirigirse mediante comunicación escrita, aportando documentación que 
+				acredite su identidad (DNI o pasaporte), a la siguiente dirección: 
+				Empresa A, Calle: X Nº Y, Código postal: Z, Ciudad: V, Provincia: W o la 
+				dirección que sea sustituida en el Registro General de Protección de Datos.
+				Dicha comunicación deberá reflejar la siguiente información: Nombre y 
+				apellidos del usuario, la petición de solicitud, el domicilio y los datos 
+				acreditativos.</p>
+				<br>
+				<p>El ejercicio de derechos deberá ser realizado por el propio usuario. 
+				No obstante, podrán ser ejecutados por una persona autorizada como 
+				representante legal del autorizado. En tal caso, se deberá aportar la 
+				documentación que acredite esta representación del interesado.</p>
+				<br><br>			
+			</section>
+			
+			<?php include('pie.php');?>		
+		
+		</div>
+	</body>
+</html>
