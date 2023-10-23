3659f0e4f0f6367c89eb052e2595e26bfbf4a65c
JavierBellon29 <javier.bellon2@alu.uclm.es>
2023-10-23 17:54:17 +0200

    imagenes de productos

--- script_imagym.sql
+++ script_imagym.sql
@@ -78,32 +78,32 @@ CREATE TABLE Productos (
     FOREIGN KEY (GeneroID) REFERENCES Generos(ID)
 );
 
-INSERT INTO Productos (Nombre, Precio, Stock, CategoriaID, GeneroID)
+INSERT INTO Productos (Nombre,Imagen, Precio, Stock, CategoriaID, GeneroID)
 VALUES
-    ('Camiseta Nike', 29.99, 100, 1, 1),
-    ('Camiseta Million', 39.99, 75, 1, 1),
-    ('Pantalón Adidas', 24.99, 50, 1, 2),
-    ('Pantalón Joma', 19.99, 15, 1, 2),
-    ('Zapatillas Asics', 69.99, 20, 1, 3),
-    ('Zapatillas Nike', 79.99, 10, 1, 3),
-    ('Mancuerna 10kg', 29.99, 20, 2, 4),
-    ('Mancuerna 15kg', 34.99, 20, 2, 4),
-    ('Goma 10kg', 5.99, 15, 2, 5),
-    ('Pack Gomas', 14.99, 40, 2, 5),
-	('Barra Z', 19.99, 25, 2, 6),
-	('Barra Olímpica', 25.99, 10, 2, 6),
-	('Cinta de Correr', 249.99, 5, 3, 7),
-	('Bicicleta Estática', 199.99, 3, 3, 7),
-	('Rack Banca', 59.99, 10, 3, 8),
-	('Rack Sentadilla', 99.99, 5, 3, 8),
-	('Máquina Poleas', 249.99, 3, 3, 9),
-	('Remo Horizontal', 149.99, 4, 3, 9),
-	('MyProtein Chocolate', 14.99, 300, 4, 10),
-	('Whey Vainilla', 14.99, 300, 4, 10),
-	('Creatina Monohidrato 500mg', 24.99, 50, 4, 11),
-	('Creatina Monohidrato 1kg', 14.99, 300, 4, 11),
-	('Pre-Entreno MyProtein', 14.99, 300, 2, 5),
-	('Pre-Entreno Million', 14.99, 300, 2, 5);
+    ('Camiseta Nike','./IMAGENES_PRODUCTOS/nike.png', 29.99, 100, 1, 1),
+    ('Camiseta Million','./IMAGENES_PRODUCTOS/million.png', 39.99, 75, 1, 1),
+    ('Pantalón Adidas','./IMAGENES_PRODUCTOS/adidas.png', 24.99, 50, 1, 2),
+    ('Pantalón Joma','./IMAGENES_PRODUCTOS/joma.png', 19.99, 15, 1, 2),
+    ('Zapatillas Asics','./IMAGENES_PRODUCTOS/asics.png', 69.99, 20, 1, 3),
+    ('Zapatillas Nike','./IMAGENES_PRODUCTOS/nikeShoes.png', 79.99, 10, 1, 3),
+    ('Mancuerna 10kg','./IMAGENES_PRODUCTOS/pesa10k.png', 29.99, 20, 2, 4),
+    ('Mancuerna 15kg','./IMAGENES_PRODUCTOS/pesa15k.png', 34.99, 20, 2, 4),
+    ('Goma con agarre','./IMAGENES_PRODUCTOS/goma_agarre.png', 5.99, 15, 2, 5),
+    ('Pack Gomas','./IMAGENES_PRODUCTOS/gomas.png', 14.99, 40, 2, 5),
+	('Barra Z','./IMAGENES_PRODUCTOS/barraZ.png', 19.99, 25, 2, 6),
+	('Barra Olímpica','./IMAGENES_PRODUCTOS/barra.png', 25.99, 10, 2, 6),
+	('Cinta de Correr','./IMAGENES_PRODUCTOS/cintacorrer.png', 249.99, 5, 3, 7),
+	('Bicicleta Estática','./IMAGENES_PRODUCTOS/bici.png', 199.99, 3, 3, 7),
+	('Rack Banca','./IMAGENES_PRODUCTOS/rackbanca.png', 59.99, 10, 3, 8),
+	('Rack Sentadilla','./IMAGENES_PRODUCTOS/racksentadilla.png', 99.99, 5, 3, 8),
+	('Máquina Poleas','./IMAGENES_PRODUCTOS/poleas.png', 249.99, 3, 3, 9),
+	('Remo Horizontal','./IMAGENES_PRODUCTOS/remo.png', 149.99, 4, 3, 9),
+	('MyProtein Chocolate','./IMAGENES_PRODUCTOS/chocolate.png', 14.99, 300, 4, 10),
+	('Whey Vainilla','./IMAGENES_PRODUCTOS/proteinvanila.png', 14.99, 300, 4, 10),
+	('Creatina Monohidrato 500mg','./IMAGENES_PRODUCTOS/creatina500.png', 24.99, 50, 4, 11),
+	('Creatina Monohidrato 1kg','./IMAGENES_PRODUCTOS/creatina1000.png', 14.99, 300, 4, 11),
+	('Pre-Entreno MyProtein','./IMAGENES_PRODUCTOS/preentreno.png', 14.99, 300, 2, 5),
+	('Pre-Entreno Million','./IMAGENES_PRODUCTOS/lambo.png', 14.99, 300, 2, 5);
 	
 
 -- Crear la tabla de facturas
