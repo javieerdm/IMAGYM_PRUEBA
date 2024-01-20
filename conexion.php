2e6f038f848f11ad8068772443016905c67a93f4
JAVIER DÍAZ MEJÍA <Javier.Diaz34@alu.uclm.es>
2024-01-20 16:48:07 +0100

    arreglo conexion.php

--- conexion.php
+++ conexion.php
@@ -1,23 +1,10 @@
 <?php
-//$conexion = new mysqli('localhost', 'root', '', 'script_imagym');
-//if ($conexion->connect_error) {
-  //  die('Error en la conexión' . $conexion->connect_error);
-//}
-//mysqli_set_charset($conexion,"utf8");
-$url = parse_url(getenv("JAWSDB_URL"));
-
-$server = $url["esilxl0nthgloe1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"];
-$username = $url["tto8891hif7ljdap"];
-$password = $url["zo25ckoe4fmvbom4"];
-$db = substr($url["path"], 1); // Elimina la barra inicial en el nombre de la base de datos
-
-$conexion = new mysqli($server, $username, $password, $db);
-
+$conexion = new mysqli('localhost', 'root', '', 'script_imagym');
 if ($conexion->connect_error) {
-    die('Error en la conexión: ' . $conexion->connect_error);
+   die('Error en la conexión' . $conexion->connect_error);
 }
+mysqli_set_charset($conexion,"utf8");
 
-mysqli_set_charset($conexion, "utf8");
 ?>
 
 
